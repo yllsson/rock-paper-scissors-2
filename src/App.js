8ca0c775263c2ebf6530dc812f5c975912e86163
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-03-03 22:23:11 +0000

    Initiate repo + create game functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [result, setResult] = useState('');
+
+  // player selection on button click
+  // computer selection
+
+  const getWinner = selection => {
+    const playerSelection = parseInt(selection);
+    const computerSelection = Math.floor(Math.random() * 3);
+    let winner = '';
+
+    if (playerSelection === computerSelection) {
+      winner = "It's a tie!";
+    } else {
+      let winningChoice = computerSelection + 1;
+
+      if (winningChoice > 2) {
+        winningChoice = 0;
+      }
+
+      if (playerSelection === winningChoice) {
+        winner = 'You won!';
+        setPlayerScore(playerScore + 1);
+      } else {
+        winner = 'You lost!';
+        setComputerScore(computerScore + 1);
+      }
+    }
+
+    console.log(playerSelection, computerSelection, winner);
+    setResult(winner);
+    return winner;
+  };
+
+  useEffect(() => {
+    console.log(result);
+  }, [result]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Rock Paper Scissors!</h1>
+
+      <button
+        className='btn'
+        value='0'
+        onClick={event => {
+          getWinner(event.target.value);
+        }}
+      >
+        Rock
+      </button>
+      <button
+        className='btn'
+        value='1'
+        onClick={event => {
+          getWinner(event.target.value);
+        }}
+      >
+        Paper
+      </button>
+      <button
+        className='btn'
+        value='2'
+        onClick={event => {
+          getWinner(event.target.value);
+        }}
+      >
+        Scissors
+      </button>
+
+      <div>
+        <span>{result}</span>
+        <h2>Score Player: {playerScore}</h2>
+        <h2>Score Computer: {computerScore} </h2>
+      </div>
     </div>
   );
 }
