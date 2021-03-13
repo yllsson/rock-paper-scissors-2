2f8c4e06387f1c84b3a8af65f45c86779044267e
annekasillitoe <anneka@contact.xyz>
2021-03-13 15:32:59 +0000

    Add PropTypes to Button component
    We want to make sure we pass in the correct props to the Button
    component. Here we set up PropTypes to make sure that any required is
    passed in as expected.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export const Button = ({ className, value, id, onClick, children }) => (
   <button className="className" value={value} id={id} onClick={onClick}>
     {children}
   </button>
 );
+
+Button.propTypes = {
+  className: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
