dd0a6c06f93b899ffbb8655e26480b49cb23e2e1
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-05-07 19:54:26 +0100

    setting up maths test

---
+++ src/utils/maths.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import { getWinner } from './maths';
+
+describe('getWinner function', () => {
+  it('returns an object', () => {
+    // insert test here
+  });
+});
